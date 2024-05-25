b85b0bd330b04193fd65397e8276d4d8a05b2e2c
Luminous <lumi9259@gmail.com>
2024-05-25 21:43:10 +0545

    adding login template

---
+++ app/src/main/java/np/com/luminoussuwal/babybuy/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package np.com.luminoussuwal.babybuy.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
