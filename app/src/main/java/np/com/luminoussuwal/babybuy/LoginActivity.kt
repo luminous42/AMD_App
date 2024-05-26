4816f40179864ebafd5cb21864f790989577cbc9
Luminous <lumi9259@gmail.com>
2024-05-26 18:56:19 +0545

    update activity bindings

--- app/src/main/java/np/com/luminoussuwal/babybuy/LoginActivity.kt
+++ app/src/main/java/np/com/luminoussuwal/babybuy/LoginActivity.kt
@@ -1,20 +1,52 @@
 package np.com.luminoussuwal.babybuy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import np.com.luminoussuwal.babybuy.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val TAG = "LoginActivity"
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        Log.i(TAG, "onCreate: ")
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume: ")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause: ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop: ")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy: ")
+    }
+
+
 }
--- app/src/main/java/np/com/luminoussuwal/babybuy/SplashActivity.kt
+++ app/src/main/java/np/com/luminoussuwal/babybuy/SplashActivity.kt
@@ -1,20 +1,61 @@
 package np.com.luminoussuwal.babybuy
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import np.com.luminoussuwal.babybuy.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //Binding to Splash Activity XML File
+        binding =ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //Navigating to Login Activity
+        Handler().postDelayed({
+
+        //Check LoggedIn State in SharedPreferences
+        val sharedPreferences =this@SplashActivity.
+        application.getSharedPreferences(
+            "login",
+            Context.MODE_PRIVATE
+            )
+
+        val isLoggedIn = sharedPreferences.getBoolean(
+            "isLoggedIn",
+            false
+        )
+
+        if(isLoggedIn){
+            val intent = Intent(
+                this@SplashActivity,
+                DashboardActivity::class.java
+            )
+            startActivity(intent)
+            finish()
         }
+        else{
+            val intent = Intent(
+                this@SplashActivity,
+                LoginActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
+        },
+            3000
+
+        )
+
     }
 }
