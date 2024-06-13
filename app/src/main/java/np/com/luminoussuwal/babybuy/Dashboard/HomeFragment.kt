5d38b01ccf45562bb90d0e7a05c0f68ec6f21c55
Luminous <lumi9259@gmail.com>
2024-06-13 23:06:27 +0545

    adding views to fragment

--- app/src/main/java/np/com/luminoussuwal/babybuy/Dashboard/HomeFragment.kt
+++ app/src/main/java/np/com/luminoussuwal/babybuy/Dashboard/HomeFragment.kt
@@ -5,21 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import np.com.luminoussuwal.babybuy.ItemAdapter
 import np.com.luminoussuwal.babybuy.R
+import np.com.luminoussuwal.babybuy.databinding.FragmentHomeBinding
 
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class HomeFragment : Fragment() {
 
+    private lateinit var binding: FragmentHomeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(
+            layoutInflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvWelcome.text = "Hello, welcome to BabyBuy!"
+
+        val items = listOf("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+        val adapter = ItemAdapter(items)
+        binding.recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerView.adapter = adapter
     }
 }
